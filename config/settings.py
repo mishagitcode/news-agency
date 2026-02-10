68b09ac92691ca9c8cd95f9f58ccaa6388b90f62
Misha Zubritsky <zubritsky.misha@gmail.com>
2026-02-10 17:24:41 +0200

    Improve settings.py and .gitignore by adding .config_env

--- config/settings.py
+++ config/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(".config_env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
