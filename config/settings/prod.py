9fbacd83efc27387ec4dc95db339e7fee162c071
Misha Zubritsky <zubritsky.misha@gmail.com>
2026-03-20 15:59:40 +0200

    Add RENDER_EXTERNAL_HOSTNAME to prod.py

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,5 @@
+import os
+
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -7,6 +9,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost'
 ]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
